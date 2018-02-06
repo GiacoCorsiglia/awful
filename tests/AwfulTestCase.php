41156165fe48e09a25f4e35c48849586aff520bd
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-02-05 20:20:39 -0800

    Add annotation for WP_UnitTestCase::factory property

--- tests/AwfulTestCase.php
+++ tests/AwfulTestCase.php
@@ -6,6 +6,9 @@ use WP_UnitTestCase;
 
 /**
  * Base class for all tests.
+ *
+ * @property-read \WP_UnitTest_Factory $factory Getter for a new factory
+ *                                              provided by WP_UnitTestCase.
  */
 class AwfulTestCase extends WP_UnitTestCase
 {

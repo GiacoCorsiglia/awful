5f9d6b79b1f43062efdb60e21b4206ab63c4b31a
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-04-14 10:46:34 -0700

    Add uuid function

--- src/functions.php
+++ src/functions.php
@@ -110,3 +110,20 @@ function is_associative(array $array): bool
 {
     return every($array, 'is_string', [], ARRAY_FILTER_USE_KEY);
 }
+
+/**
+ * Generate a random-enough v4 UUID for Awful's purposes.
+ *
+ * Be wary of using this function if you have strict randomness requirements.
+ *
+ * @see https://stackoverflow.com/a/15875555
+ *
+ * @return string A new UUID.
+ */
+function uuid(): string
+{
+    $data = random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}

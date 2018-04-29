3683bee46cac6c82410a3c2a7f2ae1a9455d8a40
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-04-29 09:17:51 -0700

    Address psalm issue

--- src/Cli/AwfulCommand.php
+++ src/Cli/AwfulCommand.php
@@ -29,11 +29,16 @@ class AwfulCommand extends Command
         }
     }
 
+    /**
+     * @return int[]
+     * @psalm-return array<int, int>
+     */
     private function allSiteIds(): array
     {
         if (!is_multisite()) {
             return [0];
         }
+        /** @psalm-var array<int,int> */
         return get_sites([
             'fields' => 'ids',
             'number' => 0,

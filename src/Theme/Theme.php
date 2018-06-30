a952ec85d94863de1fec3b0761d62abf506da84d
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-06-30 13:04:18 -0700

    Better default Theme

--- src/Theme/Theme.php
+++ src/Theme/Theme.php
@@ -1,25 +1,44 @@
 <?php
 namespace Awful\Theme;
 
+use Awful\Models\Site;
+use Awful\Models\User;
+
 abstract class Theme
 {
-    public function models(): array
+    /**
+     * @return string[]
+     * @psalm-return class-string[]
+     */
+    public function postTypes(): array
     {
         return [];
     }
 
+    /**
+     * @return string[]
+     * @psalm-return class-string[]
+     */
     public function hooks(): array
     {
         return [];
     }
 
+    /**
+     * @return string
+     * @psalm-return class-string
+     */
     public function userClass(): string
     {
-        return '';
+        return User::class;
     }
 
+    /**
+     * @return string
+     * @psalm-return class-string
+     */
     public function siteClass(): string
     {
-        return '';
+        return Site::class;
     }
 }

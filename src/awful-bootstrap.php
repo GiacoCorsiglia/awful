fa5f31569daff1d251fe3b603db6f557b7a6baa3
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-07-08 16:49:43 -0700

    Add autoloader requires statement

--- src/awful-bootstrap.php
+++ src/awful-bootstrap.php
@@ -6,11 +6,20 @@
 use Awful\Awful;
 use Awful\Providers\CoreProvider;
 
+// Activate the Composer autoloader, which enables referencing all Awful classes
+// without further `require` or `include` statements.  If you need to include
+// the autoloader earlier in WordPress' bootstrap process (likely in
+// `wp-config.php`), you can remove this line.  Otherwise, update the path here
+// to match your setup.
+require __DIR__ . '../vendor/autoload.php';
+
 /**
  * This constant short-circuits Awful's initialization to avoid database errors.
  * Set it to `true` once you have added Awful's database tables.
  */
 define('AWFUL_INSTALLED', false);
+
+// Awful's bootstrap process will activate your plugins and theme.
 Awful::bootstrap([
     // Providers.
     new CoreProvider(),

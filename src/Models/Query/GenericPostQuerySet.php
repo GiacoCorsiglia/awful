2cf6c5d1ad47f513bccdb86d1f6c500c09600286
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2018-07-11 20:13:17 -0700

    Refactor a bit

--- src/Models/Query/GenericPostQuerySet.php
+++ src/Models/Query/GenericPostQuerySet.php
@@ -16,9 +16,6 @@ class GenericPostQuerySet implements ArrayAccess, Countable, IteratorAggregate
     /** @var array */
     protected $args;
 
-    /** @var GenericPost[]|null */
-    protected $posts;
-
     /** @var Site */
     protected $site;
 
@@ -36,10 +33,6 @@ class GenericPostQuerySet implements ArrayAccess, Countable, IteratorAggregate
         return $this->filter(['perm' => $permission]);
     }
 
-    //
-    // Fetch methods.
-    //
-
     public function fetch(): array
     {
         if ($this->objects !== null) {
@@ -49,26 +42,21 @@ class GenericPostQuerySet implements ArrayAccess, Countable, IteratorAggregate
         $this->objects = [];
         $this->wpQuery = new WP_Query($this->args);
 
-        $ids = wp_list_pluck($this->wpQuery->posts, 'ID');
+        $ids = [];
+        foreach ($this->wpQuery->posts as $wpPost) {
+            $ids[] = $wpPost->ID;
+            $this->objects[$wpPost->ID] = new GenericPost(
+                $this->site,
+                $wpPost->ID
+            );
+        }
 
         if ($this->args['awful_prefetch_blocks']) {
             $blockSetManager = $this->site->entityManager()->blockSetManager();
             $blockSetManager->prefetchBlockRecords(new BlockQueryForPosts($this->site->id(), ...$ids));
         }
 
-        foreach ($this->wpQuery->posts as $wpPost) {
-            $this->posts[$wpPost->ID] = new GenericPost(
-                $this->site,
-                $wpPost->ID
-            );
-        }
-
         return $this->objects;
-    }
-
-    public function fetchByIds(int ...$ids): array
-    {
-        return $this->filter(['post__in', $ids])->fetch();
     }
 
     public function fields(string $fields): self
@@ -82,37 +70,42 @@ class GenericPostQuerySet implements ArrayAccess, Countable, IteratorAggregate
         return new static($this->site, $args + $this->args);
     }
 
-    public function metaQuery(MetaQuery $meta_query): self
+    public function ids(int ...$ids): array
     {
-        return $this->filter(['meta_query' => $meta_query->toArray()]);
+        return $this->filter(['post__in', $ids]);
     }
 
-    public function mimeType(string ...$mime_types): self
+    public function metaQuery(MetaQuery $metaQuery): self
+    {
+        return $this->filter(['meta_query' => $metaQuery->toArray()]);
+    }
+
+    public function mimeType(string ...$mimeTypes): self
     {
         assert($this->args['post_type'] === 'attachment');
-        return $this->filter(['post_mime_type' => $mime_types]);
+        return $this->filter(['post_mime_type' => $mimeTypes]);
     }
 
     public function orderBy(string ...$orders): self
     {
-        $order_arg = [];
-        $order_by_arg = [];
+        $orderArg = [];
+        $orderByArg = [];
         foreach ($orders as $order) {
             if (!$order) {
                 continue;
             }
             if ($order[0] === '-') {
                 $order = substr($order, 1);
-                $order_arg[] = 'ASC';
+                $orderArg[] = 'ASC';
             } else {
-                $order_arg[] = 'DESC';
+                $orderArg[] = 'DESC';
             }
-            $order_by_arg[] = $order;
+            $orderByArg[] = $order;
         }
 
         return $this->filter([
-            'order' => $order_arg,
-            'orderby' => $order_by_arg,
+            'order' => $orderArg,
+            'orderby' => $orderByArg,
         ]);
     }
 
@@ -145,14 +138,10 @@ class GenericPostQuerySet implements ArrayAccess, Countable, IteratorAggregate
         return $this->filter(['post_status' => $statuses]);
     }
 
-    public function taxonomyQuery(TaxonomyQuery $tax_query): self
+    public function taxonomyQuery(TaxonomyQuery $taxQuery): self
     {
-        return $this->filter(['tax_query' => $tax_query->toArray()]);
+        return $this->filter(['tax_query' => $taxQuery->toArray()]);
     }
-
-    //
-    // Filter methods.
-    //
 
     public function type(string ...$types): self
     {
@@ -167,10 +156,6 @@ class GenericPostQuerySet implements ArrayAccess, Countable, IteratorAggregate
         /** @psalm-var WP_Query */
         return $this->wpQuery;
     }
-
-    //
-    // Internal methods.
-    //
 
     protected function defaults(): array
     {
